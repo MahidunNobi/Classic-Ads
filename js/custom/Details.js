37e44504c11ac1097d7f99bb7dd777c4ada405d8
MahidunNobi <mdmahidunnobi@gmail.com>
2023-06-08 22:56:33 +0600

    csr

--- js/custom/Details.js
+++ js/custom/Details.js
@@ -8,3 +8,34 @@ leftArrow.addEventListener("click", ()=>{
 rightArrow.addEventListener("click", ()=>{
     imgUl.scrollLeft +=200
 })
+
+            const GetaQuoteBtns = document.querySelector(".btns button:nth-child(2)")
+            const form = document.querySelector(".form")
+            const allCloseFormButtons = document.querySelector(".closeFormDiv button i")
+
+            const formSubmitBtns = document.querySelector(".form .Btn button")
+            const ThankUMessageBox = document.querySelector(".ThankU")
+            const MessageCloseBtn = document.querySelector(".mainThankU .closeBtn button.closeThankUBtn")
+
+            GetaQuoteBtns.addEventListener("click", ()=>{             
+                    form.classList.add("active")
+                    window.scrollBy(0, 200)
+             
+            })
+
+            allCloseFormButtons.addEventListener("click", ()=>{
+                    form.classList.remove("active")
+                })
+          
+
+            formSubmitBtns.addEventListener("click", ()=>{
+                    ThankUMessageBox.classList.add("active")
+                    window.innerWidth<=991 && window.scrollBy(0, -400)
+                })
+         
+            MessageCloseBtn.addEventListener("click", ()=>{
+                    form.classList.remove("active")
+                ThankUMessageBox.classList.remove("active")
+                })
+                
+      
