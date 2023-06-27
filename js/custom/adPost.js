2e9c0a77d34264db2fe3b8c942df537fa5bb1e7b
MahidunNobi <mdmahidunnobi@gmail.com>
2023-06-27 20:53:52 +0600

    safsd

--- js/custom/adPost.js
+++ js/custom/adPost.js
@@ -10,3 +10,51 @@ socialLinksBtns.forEach(e=>{
     })
 })
 
+// IMAGE UPLOAD 
+let uploadedImages = []
+const inputImage = document.querySelector("input.uploadImg")
+let clsBtn = document.querySelector(".clsBtn")
+const ImagesDiv = document.querySelector(".allImages")
+const addImageDiv = document.querySelector(".singleImage.imageUploader")
+
+inputImage.addEventListener("change", ()=>{
+    const Files = inputImage.files
+    for(let i = 0; i<Files.length; i++){
+        uploadedImages.push(Files[i])
+    }
+    displayImg()
+    Max7Image()
+})
+
+function displayImg(){
+    let imagesHtml = ""
+
+    uploadedImages.forEach((e, i)=>{
+        imagesHtml += `<div class="singleImage">
+                        <span class="clsBtn" onclick="deleteItem(${i})">
+                            <i class="fa-solid fa-x"></i>
+                        </span>
+                        <img src="${URL.createObjectURL(e)}" alt="Image">
+                    </div> `
+    })
+
+   ImagesDiv.innerHTML = imagesHtml
+
+}
+
+function Max7Image(){
+    if(uploadedImages.length === 7){
+        addImageDiv.classList.add("hidden")
+    }else{
+        addImageDiv.classList.remove("hidden")
+    }
+}
+
+function deleteItem(i){
+    uploadedImages.splice(i, 1)
+    displayImg()
+    Max7Image()
+}
+
+// IMAGE UPLOAD End
+
